5f2c6e7c2762e4e420c08589059a7aa513e35988
yuma300 <ywakimoto2s@gmail.com>
2014-08-22 08:42:09 +0900

    マーケットをせんたくできるようにした

--- lib/monacoin_client/monacoin_tickers/monatr.rb
+++ lib/monacoin_client/monacoin_tickers/monatr.rb
@@ -1,8 +1,8 @@
 module MonacoinTickers
 	# Priceticker on monatr
 	module Monatr
-		def ticker
-			json = MonacoinTickers::getJsonObject("https://api.monatr.jp/ticker?market=BTC_MONA")
+		def ticker(market)
+			json = MonacoinTickers::getJsonObject("https://api.monatr.jp/ticker?market=" + market)
 		end
 		module_function :ticker
 	end
